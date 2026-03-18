b7caaa9362a91440da21cf61d654db70f7971eec
Faysal2123 <foysal2005n1@gmail.com>
2026-03-19 01:50:47 +0600

    feat: add request validation to create department route

--- src/app/modules/academicDepartment/academicDepartment.routes.ts
+++ src/app/modules/academicDepartment/academicDepartment.routes.ts
@@ -1,7 +1,15 @@
 import express from 'express'
 import { AcademicDepartmentController } from './academicDepartment.controller.js'
+import validateRequest from '../../../middlewares/validateRequest.js'
+import { AcademicDepartmentValidation } from './academicDepartment.validation.js'
 const router = express.Router()
 
-router.post('/create-department', AcademicDepartmentController.createDepartment)
+router.post(
+  '/create-department',
+  validateRequest(
+    AcademicDepartmentValidation.createAcademicDepartmentZodSchema,
+  ),
+  AcademicDepartmentController.createDepartment,
+)
 
 export const AcademicDepartmentRoutes = router
