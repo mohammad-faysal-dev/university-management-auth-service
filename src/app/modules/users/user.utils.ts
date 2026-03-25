e36da080c802e80d0e5b31c10bb23687f44ea1fc
Faysal2123 <foysal2005n1@gmail.com>
2026-03-25 23:35:04 +0600

    feat: add faculty ID generation utility

--- src/app/modules/users/user.utils.ts
+++ src/app/modules/users/user.utils.ts
@@ -20,3 +20,18 @@ export const generateStudentId = async (
   incrementedId = `${academicSemester.year.substring(2)}${academicSemester.code}${incrementedId}`
   return incrementedId
 }
+
+export const findLastFacultyId = async () => {
+  const lastFaculty = await User.findOne({}, { id: 1, _id: 0 })
+    .sort({ createdAt: -1 })
+    .lean()
+  return lastFaculty?.id
+}
+
+export const generateFacultyId = async (): Promise<string> => {
+  const currentId =
+    (await findLastFacultyId()) || (0).toString().padStart(5, '0')
+  let incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0')
+  incrementedId = `F-${incrementedId}`
+  return incrementedId
+}
